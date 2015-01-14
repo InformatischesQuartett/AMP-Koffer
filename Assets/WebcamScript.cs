cdc7fa6bb631b881a20528aafe37e761a90a5103
Alexander Scheurer <mail@aspepex.net>
2015-01-14 18:52:34 +0100

    Blub

---
+++ Assets/WebcamScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		if (devices.Length > 0) {
+			WebCamTexture myWebCamTex = new WebCamTexture();
+			myWebCamTex.Play();
+
+			renderer.material.mainTexture = myWebCamTex;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
